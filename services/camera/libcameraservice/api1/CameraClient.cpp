3ef25bd01774a6394cca4137b0aaa37c763b9c21
Apurva Rajguru <arajguru@codeaurora.org>
2012-07-12 11:34:12 -0700

    Camera: Enable QC Camera feature set.
    Change-Id: Ie52b6207293abe5a57d3bbe9d9c43e8c5b070af5
    
    Conflicts:
    
    	services/camera/libcameraservice/api1/CameraClient.cpp

--- services/camera/libcameraservice/api1/CameraClient.cpp
+++ services/camera/libcameraservice/api1/CameraClient.cpp
@@ -256,9 +256,12 @@ void CameraClient::disconnect() {
 
     // Release the held ANativeWindow resources.
     if (mPreviewWindow != 0) {
+#ifdef QCOM_HARDWARE
+        mHardware->setPreviewWindow(0);
+#endif
         disconnectWindow(mPreviewWindow);
         mPreviewWindow = 0;
-#ifndef NO_UPDATE_PREVIEW
+#if !defined(NO_UPDATE_PREVIEW) || if !defined(QCOM_HARDWARE)
         mHardware->setPreviewWindow(mPreviewWindow);
 #endif
     }
@@ -299,6 +302,10 @@ status_t CameraClient::setPreviewWindow(const sp<IBinder>& binder,
             native_window_set_buffers_transform(window.get(), mOrientation);
             result = mHardware->setPreviewWindow(window);
         }
+#ifdef QCOM_HARDWARE
+    } else {
+        result = mHardware->setPreviewWindow(window);
+#endif
     }
 
     if (result == NO_ERROR) {
@@ -362,6 +369,9 @@ status_t CameraClient::startPreview() {
     enableMsgType(CAMERA_MSG_PREVIEW_METADATA);
 #endif
     LOG1("startPreview (pid %d)", getCallingPid());
+#ifdef QCOM_HARDWARE
+    enableMsgType(CAMERA_MSG_PREVIEW_METADATA);
+#endif
     return startCameraMode(CAMERA_PREVIEW_MODE);
 }
 
@@ -593,6 +603,7 @@ status_t CameraClient::takePicture(int msgType) {
 #if defined(OMAP_ICS_CAMERA) || defined(OMAP_ENHANCEMENT_BURST_CAPTURE)
     picMsgType |= CAMERA_MSG_COMPRESSED_BURST_IMAGE;
 #endif
+
 #ifdef QCOM_HARDWARE
     disableMsgType(CAMERA_MSG_PREVIEW_METADATA);
 #endif
